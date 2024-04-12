cc68c8412210e6721a3679b27512999feb434097
Atul Chourasiya <atulchourasiya73@gmail.com>
2024-04-12 10:57:31 +0530

    Added Build

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
-const express = require('express');
-const connectToMongoose = require('./config/mongo');
+const express = require("express");
+const connectToMongoose = require("./config/mongo");
 const app = express();
-const task = require('./routes/task');
-const user = require('./routes/user');
-const path = require('path');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const task = require("./routes/task");
+const user = require("./routes/user");
+const path = require("path");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 5000;
 
 connectToMongoose();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-	express.static(path.join(__dirname, 'build'), {
-		etag: false,
-		maxAge: '1000'
-	})
+  express.static(path.join(__dirname, "build"), {
+    etag: false,
+    maxAge: "1000",
+  })
 );
 
 app.use(
-	cors({
-		origin: `${process.env.CLIENT_URL}`,
-		methods: 'GET,POST,PUT,DELETE',
-		credentials: true
-	})
+  cors({
+    origin: `${process.env.CLIENT_URL}`,
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
 );
-app.use('/task', task);
-app.use('/user', user);
+app.use("/task", task);
+app.use("/user", user);
 
-// app.get('/*', function (req, res) {
-// 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(PORT, () => {
-	console.log(`App is listening on port ${PORT}`);
+  console.log(`App is listening on port ${PORT}`);
 });
